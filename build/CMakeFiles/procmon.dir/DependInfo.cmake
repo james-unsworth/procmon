f302babc7e69a10b75cbd5fc4d68caa12d0b0186
James Unsworth <jamesunsworth00@gmail.com>
2025-11-07 12:31:05 +0000

    Create CMakeLists and build dir

---
+++ build/CMakeFiles/procmon.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/james/Projects/procmon_v.2/src/main.c" "CMakeFiles/procmon.dir/src/main.c.o" "gcc" "CMakeFiles/procmon.dir/src/main.c.o.d"
+  "/home/james/Projects/procmon_v.2/src/parser.c" "CMakeFiles/procmon.dir/src/parser.c.o" "gcc" "CMakeFiles/procmon.dir/src/parser.c.o.d"
+  "/home/james/Projects/procmon_v.2/src/sampler.c" "CMakeFiles/procmon.dir/src/sampler.c.o" "gcc" "CMakeFiles/procmon.dir/src/sampler.c.o.d"
+  "/home/james/Projects/procmon_v.2/src/snapshot.c" "CMakeFiles/procmon.dir/src/snapshot.c.o" "gcc" "CMakeFiles/procmon.dir/src/snapshot.c.o.d"
+  "" "procmon" "gcc" "CMakeFiles/procmon.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/procmon.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/procmon.dir/link.d"
+  "CMakeFiles/procmon.dir/src/main.c.o"
+  "CMakeFiles/procmon.dir/src/main.c.o.d"
+  "CMakeFiles/procmon.dir/src/parser.c.o"
+  "CMakeFiles/procmon.dir/src/parser.c.o.d"
+  "CMakeFiles/procmon.dir/src/sampler.c.o"
+  "CMakeFiles/procmon.dir/src/sampler.c.o.d"
+  "CMakeFiles/procmon.dir/src/snapshot.c.o"
+  "CMakeFiles/procmon.dir/src/snapshot.c.o.d"
+  "procmon"
+  "procmon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/procmon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
